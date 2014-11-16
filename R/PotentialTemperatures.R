3c51068840b50a948951547d619599ebbb4d1961
WilliamCooper <cooperw@ucar.edu>
2014-11-16 01:03:01 -0700

    Added Rossby and Bolton forms of equivalent potential temperature, to file EquivalentPotentialTemperature.R, to complement the Davies-Jones form that is already there.

--- R/PotentialTemperatures.R
+++ R/PotentialTemperatures.R
@@ -41,13 +41,73 @@ EquivalentPotentialTemperature <- function (P, AT, E=0) {
   L1 <- 1754.
   K2 <- 1.137e6
   TK <- AT + TZERO
-  if (E <= 0) {E <- MurphyKoop (AT)}
+  E[E <= 0] <- MurphyKoop (AT[E <= 0])
   r <- MixingRatio (E/P) 
   CP <- SpecificHeats(0.)     # need dry-air value, don't need vector
   TL = 2840./(3.5*log(TK)-log(E)-4.805)+55.
   TDL <- TK*(1000./(P-E))**0.2854*(TK/TL)**(0.28e-3*r)
   THETAP <- TDL * exp (r*(L0-L1*(TL-TZERO)+K2*r)/(CP[1]*TL))
   return (THETAP)
+}
+
+#' @title RossbyEquivalentPotentialTemperature
+#' @description The Rossby form of the pseudo-adiabatic equivalent potential temperature
+#' @details The Rossby form of the pseudo-adiabatic equivalent potential temperature is calculated according to the formula of Davies-Jones (2009); see ProcessingAlgorithms.pdf
+#' @aliases RossbyEquivalentPotentialTemperature
+#' @author William Cooper
+#' @export RossbyEquivalentPotentialTemperature
+#' @param P A numeric representing ambient pressure in hPa 
+#' @param AT A numeric representing air temperature in deg. C 
+#' @param E A numeric representing water vapor pressure in hPa. Defaults to zero,
+#' and for that case the calculation uses the equilibrium vapor pressure at AT.
+#' @return A numeric representing the Rossby-equation result for the pseudo-adiabatic equivalent potential temperature in kelvin.
+#' @examples 
+#' THETAR <- RossbyEquivalentPotentialTemperature (700., 10., 9.) 
+RossbyEquivalentPotentialTemperature <- function (P, AT, E=0) {
+  # Rossby pseudo-adiabatic equivalent potential 
+  # temperature. Needs P, AT, E (hPa, degC, hPa).
+  # (cf Saucier, p. 14)
+  TK <- AT + TZERO
+  e <- E
+  e[E <= 0] <- MurphyKoop (AT[E <= 0])
+  r <- MixingRatio (e/P) 
+  CP <- SpecificHeats(0.)     # need dry-air value, don't need vector
+  TL = 2840./(3.5*log(TK)-log(e)-4.805)+55.
+  TL[E <= 0] <- TK[E <= 0]
+  LV <- 2.501e6-2370*(TL-TZERO)
+  THETAR <- TK * (1000/(P-e))^0.2857 * exp (r*LV/(CP[1]*TL))
+  return (THETAR)
+}
+
+#' @title BoltonEquivalentPotentialTemperature
+#' @description The Bolton form of the pseudo-adiabatic equivalent potential temperature
+#' @details The Bolton form of the pseudo-adiabatic equivalent potential temperature is calculated according to the formula of Davies-Jones (2009); see ProcessingAlgorithms.pdf
+#' @aliases BoltonEquivalentPotentialTemperature
+#' @author William Cooper
+#' @export BoltonEquivalentPotentialTemperature
+#' @param P A numeric representing ambient pressure in hPa 
+#' @param AT A numeric representing air temperature in deg. C 
+#' @param E A numeric representing water vapor pressure in hPa. Defaults to zero,
+#' and for that case the calculation uses the equilibrium vapor pressure at AT.
+#' @return A numeric representing the Bolton-equation result for the pseudo-adiabatic equivalent potential temperature in kelvin.
+#' @examples 
+#' THETAB <- BoltonEquivalentPotentialTemperature (700., 10., 9.) 
+BoltonEquivalentPotentialTemperature <- function (P, AT, E=0) {
+  # Bolton pseudo-adiabatic equivalent potential 
+  # temperature. Needs P, AT, E (hPa, degC, hPa).
+  TK <- AT + TZERO
+  e <- E
+  e[E <= 0] <- MurphyKoop (AT[E <= 0])
+  r <- MixingRatio (e/P) 
+  CP <- SpecificHeats(0.)     # need dry-air value, don't need vector
+  TL = 2840./(3.5*log(TK)-log(e)-4.805)+55.
+  TL[E <= 0] <- TK[E <= 0]
+  LV <- 2.501e6-2370*(TL-TZERO)
+  RBYCPM <- 0.2857 * (1-0.608 * r) / (1 + 0.887 * r)
+  THETAB <- TK * (1000 / P)^(0.2857 * (1-0.28*r)) * 
+    exp((3.376 / TL-0.00254) * r * (1000.+810.*r))
+  
+  return (THETAB)
 }
 
 #' @title VirtualTemperature
@@ -111,17 +171,16 @@ WetEquivalentPotentialTemperature <- function (P, AT, E=0, w=0) {
 # ratio (r, dimensionless), and condensed water content (w) in
 # g/m^3
   Tk <- AT + TZERO
-  eeq <- MurphyKoop (AT, P)
-  if (E <= 0) {
-    E <- eeq
-  }
+  eeq <- MurphyKoop (AT)      # I think normal def omits enhancement factor
+  E[E <= 0] <- eeq[E <= 0]    # written this way, preserves vector ops
   Lv <- 2.501e6 - 2370.*AT    # latent heat of vaporization, temp-dependent
-  cw <- 4190.                  # J/(kg K), mean value 0-90C
-  CP <- SpecificHeats ()     # need dry-air values
-  r <- MixingRatio (E/P)
+  cw <- 4190.                 # J/(kg K), mean value 0-90C
+  CP <- SpecificHeats ()      # need dry-air values
+  r <- MixingRatio (E / P)
   rt <- r + (w/1000.) / (100 * (P-E) / (CP[3] * Tk)) # denom. is density of dry air
-  cpt <- CP[1]+rt*cw
-  F1 <- ifelse ((E < eeq), (E/eeq) ^ (-r*StandardConstant("Rw")/cpt), 1.)
-  return (F1 * Tk * (1000 / (P - E)) ^ (CP[3] / cpt) * exp((Lv * r) / (cpt * Tk)))
+  cpt <- CP[1] + rt*cw
+  F1 <- ifelse ((E < eeq), (E/eeq) ^ (-r*StandardConstant("Rw")/cpt), 1)
+  return (F1 * Tk * (1000 / (P - E)) ^ (CP[3] / cpt) 
+          * exp((Lv * r) / (cpt * Tk)))
 }
 
