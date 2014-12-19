fa3458ba76876357444ca00c9be491ed1163a88b
WilliamCooper <cooperw@ucar.edu>
2014-12-19 08:48:23 -0700

    Fixed PCorFunction for C-130, with both pressure sets included (PSFD/QCF and PSFRD/QCFR).

--- R/PCorFunction.R
+++ R/PCorFunction.R
@@ -9,23 +9,27 @@
 #' @param q Measured (uncorrected) dynamic pressure (e.g., QCF), hPa (or any units that match those used for p) -- default 100 hPa. This is unused for the C-130 so it does not need to be supplied.
 #' @param akrd Ther angle of attack (degrees; no default)
 #' @param AC Aircraft identifier, either "GV" of "C130" (default "GV")
+#' @param Sensors Normally "STD" (the default) but "R" for right pressure pair, C130 
 #' @return The ratio of the pressure correction to the measured pressure. The correction to dynamic pressure is the negative of the returned value.
 #' @examples 
 #' \dontrun{dPoverP <- PCorFunction (p, q, akrd, "GV")}
-PCorFunction <- function (p, q=100., akrd=2.5, AC="GV") {
-
+PCorFunction <- function (p, q=100., akrd=2.5, AC="GV", Sensors = "STD") {
+  Mach <- MachNumber(p, q)   # calculated from raw measurements
   if ((AC == "C130") | (AC == "C-130")) { # this is for PSFD; PSFRD is different
-#     b0 <- -0.005902  
-#     b1 <- 0.001344
-#     b2 <- 0.01349
-#     dp <- b0 + b1*alpha + b2*Mach
-    # fix this
-    dp <- 0.
+    if (Sensors == "R") {
+      b0 = -0.00754
+      b1 <- 0.000497
+      b2 <- 0.0368
+    } else {
+      b0 <- -0.00637  
+      b1 <- 0.001366
+      b2 <- 0.0149
+    }  
+    dp <- b0 + b1*akrd + b2*Mach
   } else {             # GV:
     #bp <- c(4.604, 18.67, 6.49)             # old AKRD coefficients for PREDICT
     #b <-  c(4.34685, 20.10448, 1.36492)     # AKRD coefficients for CONTRAST
     a <- c(-0.012255, 0.075372, -0.087508, 0.002148)       # PCOR coefficients
-    Mach <- MachNumber(p, q)   # calculated from raw measurements
     dp <- a[1] + a[2] * (q/p) + a[3] * Mach**3 + a[4] * akrd
   } 
   return (dp)
