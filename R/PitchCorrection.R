2f294ae85b7dfd22c9d543c9fc45a7143f6260f8
WilliamCooper <cooperw@ucar.edu>
2015-11-01 10:12:19 -0700

    Corrected PitchCorrection function, with addition of roll correction also.

--- R/PitchCorrection.R
+++ R/PitchCorrection.R
@@ -17,86 +17,86 @@
 #' @author William Cooper
 #' @export CorrectPitch
 #' @param D a data.frame containing at least these variables: 
-#' VNS, VEW, GGVNS, GGVEW, LAT, GGALT, THDG, PITCH, ROLL
+#' VNS, VEW, GGVNS, GGVEW, LATC, GGALT, THDG, PITCH, ROLL
 #' @import zoo signal
-#' @return PitchError -- the estimated error in the pitch [deg], which
-#' should be subtracted from PITCH to get the corrected value.
+#' @return c(PitchError, RollError) -- the estimated errors in the 
+#' pitch and roll angles [deg], which should be subtracted from 
+#' PITCH and ROLL to get the corrected values.
 #' @examples 
-#' \dontrun{PITCHC <- PITCH - CorrectPitch(D)}
+#' \dontrun{PITCHC <- PITCH - CorrectPitch(D)[, 1]}
 CorrectPitch <- function (D) {
   Cradeg <- pi/180
   ## get the data rate
   data.rate <- 1
   if ((D$Time[2]-D$Time[1]) <= 0.04) {data.rate <- 25}
   if ((D$Time[2]-D$Time[1]) <= 0.02) {data.rate <- 50}
+  LD <- nrow(D)
   ## for HR, extract a 1-Hz data.frame and work with that, then interpolate/smooth
   if (data.rate > 1) {
-    LD <- nrow(D)
     D <- D[(as.numeric(D$Time) %% 1) < 0.01,]
   }
-  .vns <- zoo::na.approx (as.vector(D$VNS), maxgap=1000, na.rm = FALSE)
-  .vew <- zoo::na.approx (as.vector(D$VEW), maxgap=1000, na.rm = FALSE)
-  .ggvns <- zoo::na.approx (as.vector(D$GGVNS), maxgap=1000, na.rm = FALSE)
-  .ggvew <- zoo::na.approx (as.vector(D$GGVEW), maxgap=1000, na.rm = FALSE)
+  MaxGap <- 1000
+  .vns <- zoo::na.approx (as.vector(D$VNS), maxgap=MaxGap, na.rm = FALSE)
+  .vew <- zoo::na.approx (as.vector(D$VEW), maxgap=MaxGap, na.rm = FALSE)
+  .ggvns <- zoo::na.approx (as.vector(D$GGVNS), maxgap=MaxGap, na.rm = FALSE)
+  .ggvew <- zoo::na.approx (as.vector(D$GGVEW), maxgap=MaxGap, na.rm = FALSE)
   .vns[is.na(.vns)] <- 0
   .vew[is.na(.vew)] <- 0
   .ggvns[is.na(.ggvns)] <- 0
   .ggvew[is.na(.ggvew)] <- 0
   # 1013 points (must be odd) to span about 1/5 Schuler osc. -- about 16.8 min
-  vndot <- signal::sgolayfilt (.vns-.ggvns, 3, 1013, m=1)  # m=1 for first deriv.
-  vedot <- signal::sgolayfilt (.vew-.ggvew, 3, 1013, m=1)
-  deltaPitchL <- -vndot/Ranadu::Gravity (D$LAT, D$GGALT)
-  deltaRollL  <- -vedot/Ranadu::Gravity (D$LAT, D$GGALT)
-  LHDG <- length(D$THDG)
-  #  .hdg <- D$THDG*Cradeg
-  #  deltaPitch <- (sin(.hdg)*deltaRollL + cos(.hdg)*deltaPitchL)/Cradeg
-  #  deltaRoll <- (cos(.hdg)*deltaRollL - sin(.hdg)*deltaPitchL)/Cradeg
-  ## replace with the full transformation matrix:
-  deltaPitch2 <- vector ("numeric", LHDG)
-  deltaRoll2  <- vector ("numeric", LHDG)
-  for (i in 1:LHDG) {
-    bl <- c(-sin(deltaPitchL[i]), sin(deltaRollL[i]), 
-            sqrt(1.-sin(deltaRollL[i])^2 - sin(deltaPitchL[i])^2))
-    bb <- as.vector(XformLB (bl, 0, 0, D$THDG[i], .reverse=FALSE))
-    deltaPitch2[i] <- -atan(bb[1]/bb[3]) / Cradeg 
-    deltaRoll2[i]  <- atan(bb[2]/bb[3]) / Cradeg 
-  }
+  NAV <- 1013
+  vndot <- signal::sgolayfilt (.vns-.ggvns, 3, NAV, m=1)  # m=1 for first deriv.
+  vedot <- signal::sgolayfilt (.vew-.ggvew, 3, NAV, m=1)
+  deltaPitchL <- -vndot/Ranadu::Gravity (D$LATC, D$GGALT)
+  deltaRollL  <- vedot/Ranadu::Gravity (D$LATC, D$GGALT)
+  .hdg <- D$THDG*Cradeg
+  deltaPitch <- (sin(.hdg)*deltaRollL + cos(.hdg)*deltaPitchL)/Cradeg
+  deltaRoll <- (cos(.hdg)*deltaRollL - sin(.hdg)*deltaPitchL)/Cradeg
   if (data.rate > 1) {
     PC <- vector ("numeric", LD)
-    L <- length(deltaPitch2)
+    RC <- vector ("numeric", LD)
+    L <- length(deltaPitch)
     for (i in 1:(L-1)) {
       for (j in 0:(data.rate-1)) {
-        PC[(i-1)*data.rate+j+1] <- deltaPitch2[i]+
-                                   (j/data.rate)*(deltaPitch2[i+1]-deltaPitch2[i])
+        PC[(i-1)*data.rate+j+1] <- deltaPitch[i]+
+                                   (j/data.rate)*(deltaPitch[i+1]-deltaPitch[i])
+        RC[(i-1)*data.rate+j+1] <- deltaRoll[i]+
+                                   (j/data.rate)*(deltaRoll[i+1]-deltaRoll[i])
       }
     }
 #     for (j in 0:(data.rate-1)) {
 #       PC[L*data.rate+j+1] <- deltaPitch2[L]
 #     }    
-    return(PC)
+    C <- c(PD, RC)
   } else {
-    return (deltaPitch2)
+    C <- c(deltaPitch, deltaRoll)
   }
+  dim(C) <- c(LD, 2)
+  return (C)
 }
 
-## This function is needed by the function CorrectPitch(). 
-## It transforms a vector in the local frame to one in the body frame
+## This function is no longer needed by the function CorrectPitch(). 
+## It is kept here for reference and for other uses.
+## It transforms a vector in the body frame to one in the local frame
 ## (or the reverse if .reverse=TRUE).
 XformLB <- function (bvector, .roll, .pitch, .heading, .reverse=FALSE) {
   Cradeg <- pi/180
   sr <- sin(.roll*Cradeg); cr <- cos(.roll*Cradeg)
   sp <- sin(.pitch*Cradeg); cp <- cos(.pitch*Cradeg)
   sh <- sin(.heading*Cradeg); ch <- cos(.heading*Cradeg)
-  if (.reverse) {
-    M <- c(cp*ch, sh*cp, -sp,
-           ch*sp*sr-sh*cr, sh*sp*sr+ch*cr, cp*sr,
-           ch*sp*cr+ch*cr, ch*cp*cr-ch*sr, cp*cr)
+  if (.reverse) {   # note that entries are in column order
+    M <- c(sh*cr+ch*sp*sr, ch*cr-sh*sp*sr, -cp*sr,
+           ch*cp, -sh*cp, sp,
+           -sh*sr+ch*sp*cr, -ch*sr-sh*sp*cr, -cp*cr)
   } else {
-    M <- c(ch*cp, sh*cr+ch*sp*sr, sh*sr-ch*sp*cr,
-           -sh*cp, ch*cr-sh*sp*sr, ch*sr+sh*sp*cr,
-           sp, -cp*sr, cp*cr)
+    
+    M <- c(sh*cp, ch*cr+sh*sp*sr, ch*sr-sh*sp*cr,
+           ch*cp, -sh*cr+ch*sp*sr, -sh*sr-ch*sp*cr,
+           -sp, cp*sr, cp*cr)
   }
   dim (M) <- c(3,3)
   return (M %*% bvector)
+  
 }
 
